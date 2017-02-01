c73830ba36f5f5c772f4cb768a3e23a16eae1ba1
Dan <Dan>
2017-02-01 23:15:55 +0100

    Real finish of setup.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApplication{
     public class Program{
         public static void Main(string[] args){
-            int a = 1;
-            string b = "boob";
-            Console.WriteLine(b);
+            string text = "I like eating ice cream. I also like drinking tea.";
+            Console.WriteLine(text);
+        }
+        public void readText(string text){
+            
+        }
+    }
+
+    public class nGram{
+        public string gram;
+        public List<charNum> probabilities;
+        public nGram(string gram){
+           this.gram = gram;
+           this.probabilities = new List<charNum>();
+        }
+        //TODO: Overloads
+    }
+
+    public class charNum{
+        public char character;
+        public int number;
+        public charNum(char character, int number = 1){
+           this.character = character;
+           this.number = number;
         }
     }
 }

6f65f13882700b2812a1ab8ee4c4982fbaf1bf6f
Dan <Dan>
2017-02-01 22:17:21 +0100

    The beginning

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ConsoleApplication{
+    public class Program{
+        public static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+

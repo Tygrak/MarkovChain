6adf4acc57dc6b929aabfacd60c0d214beafbe13
Tygrak <S-KlepacD@seznam.cz>
2017-02-23 20:39:16 +0100

    Word ngrams added, sharpparser is a new dependency

--- Program.cs
+++ Program.cs
@@ -1,28 +1,72 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
+using SharpParser;
 
 namespace ConsoleApplication{
     public class Program{
         public static void Main(string[] args){
-            string path = Directory.GetCurrentDirectory()+"\\markov_input.txt";
+            /*string path = Directory.GetCurrentDirectory()+"\\markov_input.txt";
             var inputText = File.ReadAllLines(path);
-            string text = string.Join(" ", inputText);
-            //Console.WriteLine(text);
-            markovText mText = new markovText(5, text);
-            Console.WriteLine(mText.generateText(420));
+            string text = string.Join(" ", inputText);*/
+            List<string> links = new List<string>(readLinks());
+            string text = markovText.readText();
+            string url = "https://www.explainxkcd.com/wiki/index.php/";
+            for (int i = 1; i <= 100; i++){
+                string nextUrl = url + i.ToString();
+                if(links.Contains(nextUrl)){
+                    continue;
+                } else{
+                    links.Add(nextUrl);
+                }
+                Console.WriteLine("Currently parsing: "+nextUrl);
+                page pag = new page(nextUrl);
+                int pos = pag.html.IndexOf("Trivia");
+                pag = new page(url, pag.html.Split(new string[]{"<span class=\"mw-headline\" id=\"Trivia\">Trivia</span>"}, StringSplitOptions.None)[0]);
+                section[] transcript = pag.findAllSections("dl");
+                for (int j = 0; j < transcript.Length; j++){
+                    text += transcript[j].content;
+                }
+            }
+            saveLinks(links.ToArray());
+            markovText.saveText(text);
+            markovText mText = new markovText(2, text, true);
+            Console.WriteLine(mText.generateText(100));
+        }
+
+        public static void saveLinks(string[] links){
+            string path = Directory.GetCurrentDirectory()+"\\links.txt";
+            File.WriteAllLines(path, links);
+        }
+
+        public static string[] readLinks(){
+            string path = Directory.GetCurrentDirectory()+"\\links.txt";
+            return File.ReadAllLines(path);
         }
     }
 
     public class markovText{
         public int size;
+        public bool usesWords = false;
         public string inputText;
         public List<ngram> ngrams = new List<ngram>();
 
-        public markovText(int size, string inputText){
+        public markovText(int size, string inputText, bool usesWords = false){
             this.size = size;
             this.inputText = inputText;
+            this.usesWords = usesWords;
             createNgrams(size, inputText);
+        }
+
+        public static void saveText(string text){
+            string path = Directory.GetCurrentDirectory()+"\\input.txt";
+            File.WriteAllText(path, text);
+        }
+
+        public static string readText(){
+            string path = Directory.GetCurrentDirectory()+"\\input.txt";
+            return File.ReadAllText(path);
         }
 
         public void printGrams(){
@@ -38,19 +82,49 @@ namespace ConsoleApplication{
             inputText = inputText.ToLower();
             inputText = inputText.Replace("„", "");
             inputText = inputText.Replace("“", "");
-            for (int i = 0; i < inputText.Length-size-1; i++){
-                string gram = inputText.Substring(i, size);
-                string nextString = inputText.Substring(i+size,1);
-                ngram foundGram = ngrams.Find(x => x.gram.Equals(gram));
-                if(foundGram != null){
-                    stringNum foundString = foundGram.nextStrings.Find(x => x.str.Equals(nextString));
-                    if(foundString != null){
-                        foundString.number += 1;
-                    } else{
-                        foundGram.nextStrings.Add(new stringNum(nextString));
+            inputText = inputText.Replace("\n", "");
+            List<string> gram = new List<string>();
+            if(usesWords){
+                string[] words = inputText.Split(new string[] {" "}, StringSplitOptions.None);
+                for (int i = 0; i < words.Length-size; i++){
+                    for (int j = 0; j < size; j++){
+                        gram.Add(words[i+j]);
                     }
-                } else{
-                    ngrams.Add(new ngram(gram, nextString));
+                    string nextString = words[i+size];
+                    string[] gramA = gram.ToArray();
+                    ngram foundGram = ngrams.Find(x => Enumerable.SequenceEqual(gramA, x.gram));
+                    if(foundGram != null){
+                        stringNum foundString = foundGram.nextStrings.Find(x => x.str.Equals(nextString));
+                        if(foundString != null){
+                            foundString.number += 1;
+                        } else{
+                            foundGram.nextStrings.Add(new stringNum(nextString));
+                        }
+                    } else{
+                        ngrams.Add(new ngram(gramA, nextString));
+                    }
+                    gram.Clear();
+                }
+            } else{
+                for (int i = 0; i < inputText.Length-size-1; i++){
+                    string gramS = inputText.Substring(i, size);
+                    string nextString = inputText.Substring(i+size,1);
+                    for (int j = 0; j < gramS.Length; j++){
+                        gram.Add(gramS[j].ToString());
+                    }
+                    string[] gramA = gram.ToArray();
+                    ngram foundGram = ngrams.Find(x => Enumerable.SequenceEqual(gramA, x.gram));
+                    if(foundGram != null){
+                        stringNum foundString = foundGram.nextStrings.Find(x => x.str.Equals(nextString));
+                        if(foundString != null){
+                            foundString.number += 1;
+                        } else{
+                            foundGram.nextStrings.Add(new stringNum(nextString));
+                        }
+                    } else{
+                        ngrams.Add(new ngram(gramA, nextString));
+                    }
+                    gram.Clear();
                 }
             }
         }
@@ -60,16 +134,39 @@ namespace ConsoleApplication{
             Random rand = new Random();
             int num = rand.Next(0,ngrams.Count-1);
             ngram currGram = ngrams[num];
-            text += currGram.gram;
-            for (int i = 0; i < length-size; i++){
-                num = rand.Next(0,currGram.nextCount());
-                text += currGram.nextGet(num);
-                string nextGram = text.Substring(text.Length-size);
-                currGram = ngrams.Find(x => x.gram.Equals(nextGram));
-                if(currGram == null){
-                    Console.WriteLine(nextGram);
-                    num = rand.Next(0,ngrams.Count-1);
-                    currGram = ngrams[num];
+            if(usesWords){
+                text += string.Join(" ", currGram.gram);
+                for (int i = 0; i < length-size; i++){
+                    num = rand.Next(0,currGram.nextCount());
+                    text += " " + currGram.nextGet(num);
+                    string[] words = text.Split(new string[] {" "}, StringSplitOptions.None);
+                    List<string> listGram = new List<string>();
+                    for (int j = words.Length-size; j < words.Length; j++){
+                        listGram.Add(words[j]);
+                    }
+                    string[] nextGram = listGram.ToArray();
+                    currGram = ngrams.Find(x => Enumerable.SequenceEqual(nextGram, x.gram));
+                    if(currGram == null){
+                        num = rand.Next(0,ngrams.Count-1);
+                        currGram = ngrams[num];
+                    }
+                }
+            } else{
+                text += string.Join("", currGram.gram);
+                for (int i = 0; i < length-size; i++){
+                    num = rand.Next(0,currGram.nextCount());
+                    text += currGram.nextGet(num);
+                    string stringGram = text.Substring(text.Length-size);
+                    List<string> listGram = new List<string>();
+                    for (int j = 0; j < stringGram.Length; j++){
+                        listGram.Add(stringGram[j].ToString());
+                    }
+                    string[] nextGram = listGram.ToArray();
+                    currGram = ngrams.Find(x => Enumerable.SequenceEqual(nextGram, x.gram));
+                    if(currGram == null){
+                        num = rand.Next(0,ngrams.Count-1);
+                        currGram = ngrams[num];
+                    }
                 }
             }
             return text;
@@ -77,14 +174,14 @@ namespace ConsoleApplication{
     }
 
     public class ngram{
-        public string gram;
+        public string[] gram;
         public List<stringNum> nextStrings;
 
-        public ngram(string gram){
+        public ngram(string[] gram){
            this.gram = gram;
            this.nextStrings = new List<stringNum>();
         }
-        public ngram(string gram, string nextString){
+        public ngram(string[] gram, string nextString){
            this.gram = gram;
            this.nextStrings = new List<stringNum>();
            nextStrings.Add(new stringNum(nextString));
